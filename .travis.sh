6e618d5c6e4db0144494b1e1c2d324e2a110746a
Pim Otte <potte@quintor.nl>
2018-07-09 10:34:44 +0200

    Providing test pool ip

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e 
 cd v0.3
-docker-compose up --build --force-recreate --exit-code-from tests
+TEST_POOL_IP=127.0.0.1 docker-compose up --build --force-recreate --exit-code-from tests
